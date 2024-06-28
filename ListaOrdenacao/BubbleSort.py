7830c8a77dacc7c78c9c04c98ed07e211a5d6ed0
fatec <mvlservicosinfo@gmail.com>
2024-06-28 13:06:17 -0300

    "Aletaracao Bubble Sort"

--- ListaOrdenacao/BubbleSort.py
+++ ListaOrdenacao/BubbleSort.py
@@ -5,19 +5,14 @@ class ordnance_BubbleSort():
     def __init__(self, lista):
         self.lista = lista
 
-    def __get_lista(self):
-        return self.lista
 
-    def __set_lista(self, lista):
-        self.lista = lista
 
     def BubbleSeort(self):
-        lista = self.__get_lista()
-        n = len(lista)
+        n = len(self.lista)
         for i in range(n):
             for j in range(0, n - i - 1):
-                if lista[j] > lista[j + 1]:
-                    lista[j], lista[j + 1] = lista[j + 1], lista[j]
-                    print(f"Troca:{lista}")
-                    print(f"Passada: {i + 1}: {lista}")
-        return lista
+                if self.lista[j] > self.lista[j + 1]:
+                    self.lista[j], self.lista[j + 1] = self.lista[j + 1], self.lista[j]
+                    print(f"Troca:{self.lista}")
+                    print(f"Passada: {i + 1}: {self.lista}")
+        return self.lista
