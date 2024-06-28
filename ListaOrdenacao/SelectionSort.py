39c417633b32a51d78421bc1f2a8aede126d91cc
fatec <mvlservicosinfo@gmail.com>
2024-06-28 11:16:18 -0300

    "Alteração Geral Selection Sort"

---
+++ ListaOrdenacao/SelectionSort.py
@@ -0,0 +1,27 @@
+class SelectionSort(object):
+    def __init__(self, lista):
+        self.lista = lista
+
+    def __get_lista(self):
+        return self.lista
+
+    def __set_lista(self, lista):
+        self.lista = lista
+
+    def selection_sort(self):
+        if not self.lista:
+            return self.lista
+        for i in range(len(self.lista)):
+            min = i
+            for j in range(i + 1, len(self.lista)):
+                if self.lista[j] < self.lista[min]:
+                    min = j
+            self.lista[i], self.lista[min] = self.lista[min], self.lista[i]
+            print(f"Trocado: {i+1} : {self.lista}")
+        return self.lista
+
+
+
+
+
+
